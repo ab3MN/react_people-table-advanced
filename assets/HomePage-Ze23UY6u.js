958e9dbe9b4152028cdb7b5b083032ce51c1012d
ab3MN <ab3mn3@gmail.com>
2024-11-19 09:50:42 +0200

    Updates

---
+++ assets/HomePage-Ze23UY6u.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D3sAXgrQ.js";import{T as e}from"./Title-CI5eyIiN.js";const s=()=>t.jsx(e,{text:"Home Page"});export{s as default};
---
+++ assets/NotFoundPage-BhPk3ZUE.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D3sAXgrQ.js";import{T as o}from"./Title-CI5eyIiN.js";const a=()=>t.jsx(o,{text:"Page not found"});export{a as default};
